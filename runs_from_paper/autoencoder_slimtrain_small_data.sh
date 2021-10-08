e606d9f0524061ce851adc7b3a1b24736623046a
elizabethnewman <elizabeth.newman@emory.edu>
2021-10-08 09:34:23 -0400

    fixed typo

--- runs_from_paper/autoencoder_slimtrain_small_data.sh
+++ runs_from_paper/autoencoder_slimtrain_small_data.sh
@@ -2,7 +2,7 @@
 export PYTHONPATH=".."
 
 num_epochs=50
-alpha1=1e-10
+alpha=1e-10
 num_val=10000
 num_test=10000
 log_interval=10
@@ -13,7 +13,7 @@ for num_train in 128 256 512 1024 2048 4096 8192 16384
 do
   for seed in 0 10 20 30 40 50 60 70 80 90
   do
-      python run_autoencoder_mnist_slimtrain.py --num-epoch $num_epochs --batch-size $batch_size --num-train $num_train --num-val $num_val --num-test $num_test --log-interval $log_interval --alpha1 $alpha1 --alpha2 $alpha2 --mem-depth $mem_depth --sum-lambda $alpha2 --seed $seed --save
+      python run_autoencoder_mnist_slimtrain.py --num-epoch $num_epochs --batch-size $batch_size --num-train $num_train --num-val $num_val --num-test $num_test --log-interval $log_interval --alpha1 $alpha --alpha2 $alpha --mem-depth $mem_depth --sum-lambda $alpha --seed $seed --save
   done
 done
 
