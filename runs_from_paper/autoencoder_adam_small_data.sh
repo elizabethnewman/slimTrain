86f4e1744b507f3e516a1ccfdc6f67774dac52cc
elizabethnewman <elizabeth.newman@emory.edu>
2021-10-07 22:56:50 -0400

    fixed scripts

--- runs_from_paper/autoencoder_adam_small_data.sh
+++ runs_from_paper/autoencoder_adam_small_data.sh
@@ -10,9 +10,12 @@ num_test=10000
 log_interval=10
 batch_size=32
 
-for seed in 0 10 20 30 40 50 60 70 80 90
+for num_train in 128 256 512 1024 2048 4096 8192 16384
 do
+  for seed in 0 10 20 30 40 50 60 70 80 90
+  do
     python run_autoencoder_mnist_adam.py --num-epoch $num_epochs --batch-size $batch_size --num-train $num_train --num-val $num_val --num-test $num_test --log-interval $log_interval --alpha1 $alpha1 --alpha2 $alpha2 --seed $seed --save
+  done
 done
 
 
