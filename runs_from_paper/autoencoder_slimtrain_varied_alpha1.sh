9d095ad403217743d5e3fbeb8e3fc7fdba48dda9
elizabethnewman <elizabeth.newman@emory.edu>
2021-10-08 21:41:42 -0400

    fixed typo

--- runs_from_paper/autoencoder_slimtrain_varied_alpha1.sh
+++ runs_from_paper/autoencoder_slimtrain_varied_alpha1.sh
@@ -12,7 +12,7 @@ seed=20
 
 for alpha in 1e-10 1e-5 1e-3 1e-1 1e0
 do
-    python run_autoencoder_mnist_slimtrain.py --num-epoch $num_epochs --batch-size $batch_size --num-train $num_train --num-val $num_val --num-test $num_test --log-interval $log_interval --alpha1 $alpha --alpha2 $alpha --mem-depth $mem_depth --sum-lambda $alpha2 --seed $seed --save
+    python run_autoencoder_mnist_slimtrain.py --num-epoch $num_epochs --batch-size $batch_size --num-train $num_train --num-val $num_val --num-test $num_test --log-interval $log_interval --alpha1 $alpha --alpha2 $alpha --mem-depth $mem_depth --sum-lambda $alpha --seed $seed --save
 done
 
 
